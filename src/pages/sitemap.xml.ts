608a6cb8ff1434f3ac23f0e3fb97208a5878be48
mercerlanepress <talwrn79@yahoo.co.uk>
2026-09-22 08:39:41 +0100

    Add rescue dog pages to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -11,6 +11,7 @@ export const GET: APIRoute = async () => {
   const dogEnrichmentGuides = await getCollection('dogEnrichmentGuides');
   const cooperativeCareGuides = await getCollection('cooperativeCareGuides');
   const rescueDogFirst90DaysGuides = await getCollection('rescueDogFirst90DaysGuides');
+  const rescueDogFirst90DaysGuides = await getCollection('rescueDogFirst90DaysGuides');
   const heatPumpOwnerGuides = await getCollection('heatPumpOwnerGuides');
   const powerAutomateGuides = await getCollection('powerAutomateGuides');
   const paths = [
